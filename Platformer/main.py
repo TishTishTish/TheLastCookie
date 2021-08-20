4a34c9fa4a46a717db0ecacd749082f81c0aa5b2
Ahartisha Selakanabarajah <arty_rajah@outlook.com>
2021-08-20 22:10:40 +0100

    New changes

--- Platformer/main.py
+++ Platformer/main.py
@@ -17,6 +17,8 @@ MAP_SCALING = 1.0
 GRAVITY = 1.0
 PLAYER_START_X = 65
 PLAYER_START_Y = 256
+PLAYER_MOVE_SPEED = 10
+PLAYER_JUMP_SPEED = 20
 
 # Path to the assets folder
 ASSETS_PATH = pathlib.Path(__file__).resolve().parent.parent / "Platformer" / "assets"
@@ -38,6 +40,9 @@ class Platformer(arcade.Window):
         # Player Sprite
         self.player = None
         
+        # Physics Engine
+        self.physics_engine = None
+        
         # Score
         self.score = 0
         
@@ -45,8 +50,8 @@ class Platformer(arcade.Window):
         self.level = 1
         
         # Sounds
-        #self.cookie_sound = arcade.load_sound(str(ASSETS_PATH / "audio" / "sounds" / "select.mp3"))
-        #self.jump_sound = arcade.load_sound(str(ASSETS_PATH / "audio" / "sounds" / "jump.mp3"))
+        # self.cookie_sound = arcade.load_sound(str(ASSETS_PATH / "audio" / "sounds" / "select.mp3"))
+        # self.jump_sound = arcade.load_sound(str(ASSETS_PATH / "audio" / "sounds" / "jump.mp3"))
     
     # Set Up game
     def setup(self) -> None:
@@ -76,7 +81,7 @@ class Platformer(arcade.Window):
         if game_map.background_color:
             background_colour = game_map.background_color
         arcade.set_background_color(background_colour)
-        
+
         # If not already set up, create the player sprite
         if not self.player:
             self.player = self.create_player_sprite()
@@ -85,7 +90,7 @@ class Platformer(arcade.Window):
         self.player.centre_x = PLAYER_START_X
         self.player.centre_y = PLAYER_START_Y
         self.player.change_x = 0
-        self.player.change_x = 0
+        self.player.change_y = 0
         
         # Load up the physics engine
         self.physics_engine = arcade.PhysicsEnginePlatformer(
@@ -103,11 +108,14 @@ class Platformer(arcade.Window):
         
         # Set up textures
         walking_paths = [texture_path / f"walking_to_right{x}.png" for x in (1, 2)]
+        climbing_paths = [texture_path / f"climbing_{x}.png" for x in (1, 2)]
         standing_path = texture_path / "standing_l.png"
         
         # Load all the textures
         walking_right_textures = [arcade.load_texture(texture) for texture in walking_paths]
         walking_left_textures = [arcade.load_texture(texture, mirrored=True) for texture in walking_paths]
+        walking_up_textures = [arcade.load_texture(texture) for texture in climbing_paths]
+        walking_down_textures = [arcade.load_texture(texture) for texture in climbing_paths]
         standing_right_textures = [arcade.load_texture(standing_path)]
         standing_left_textures = [arcade.load_texture(standing_path, mirrored=True)]
         
@@ -119,6 +127,8 @@ class Platformer(arcade.Window):
         player.stand_right_textures = standing_right_textures
         player.walk_left_textures = walking_left_textures
         player.walk_right_textures = walking_right_textures
+        player.walk_up_textures = walking_up_textures
+        player.walk_down_textures = walking_down_textures
         
         # Set up the player defaults
         player.centre_x = PLAYER_START_X
@@ -131,19 +141,88 @@ class Platformer(arcade.Window):
         return player
     
     # Process key presses
-    def on_key_press(self, symbol: int, modifiers: int):
-        # return super().on_key_press(symbol, modifiers)
-        pass
+    def on_key_press(self, key: int, modifiers: int) -> None:
+        # Left/right movement
+        if key in [arcade.key.LEFT, arcade.key.J]:
+            self.player.change_x = -PLAYER_MOVE_SPEED
+        elif key in [arcade.key.RIGHT, arcade.key.L]:
+            self.player.change_x = PLAYER_MOVE_SPEED
+
+        # Climb up or down
+        elif key in [arcade.key.UP, arcade.key.I]:
+            if self.physics_engine.is_on_ladder():
+                self.player.change_y = PLAYER_MOVE_SPEED
+        elif key in [arcade.key.DOWN, arcade.key.K]:
+            if self.physics_engine.is_on_ladder():
+                self.player.change_y = -PLAYER_MOVE_SPEED
+
+        # Check if we can jump
+        elif key == arcade.key.SPACE:
+            if self.physics_engine.can_jump():
+                self.player.change_y = PLAYER_JUMP_SPEED
+                # Play the jump sound
+                arcade.play_sound(self.jump_sound)
     
     # Process key releases
-    def on_key_release(self, symbol: int, modifiers: int):
-        # return super().on_key_release(symbol, modifiers)
-        pass
+    def on_key_release(self, key: int, modifiers: int):
+        # Check for player left/right movement
+        if key in [
+            arcade.key.LEFT,
+            arcade.key.J,
+            arcade.key.RIGHT,
+            arcade.key.L,
+        ]:
+            self.player.change_x = 0
+
+        # Check if player can climb up or down
+        elif key in [
+            arcade.key.UP,
+            arcade.key.I,
+            arcade.key.DOWN,
+            arcade.key.K,
+        ]:
+            if self.physics_engine.is_on_ladder():
+                self.player.change_y = 0
     
     # Update the state of the game and all the objects in it
-    def on_update(self, delta_time: float):
-        # return super().on_update(delta_time)
-        pass
+    def on_update(self, delta_time: float) -> None:
+        # Update the player animation
+        self.player.update_animation(delta_time)
+        
+        # Update player movement based on the physics engine
+        # self.physics_engine.update()
+        
+        # Restrict player movement
+        if self.player.left < 0:
+            self.player.left = 0
+            
+        # Check if a cookie is collected
+        cookies_hit = arcade.check_for_collision_with_list(
+            sprite=self.player, sprite_list=self.cookies
+        )
+        
+        for cookie in cookies_hit:
+            # Add cookie score to our score
+            self.score += int(cookie.properties["point_value"])
+            
+            # Play the cookie sound
+            arcade.play_sound(self.cookie_sound)
+            
+            # Remove the cookie
+            cookie.remove_from_sprite_lists()
+            
+        # Check for ending goal i.e. the door
+        goals_hit = arcade.check_for_collision_with_list(
+            sprite=self.player, sprite_list=self.goals
+        )
+        
+        if goals_hit:
+            # Play victory sound
+            # self.victory_sound.play()
+            
+            # Setup the next level
+            self.level += 1
+            self.setup()
     
     def on_draw(self) -> None:
         arcade.start_render()
@@ -160,3 +239,4 @@ if __name__ == "__main__":
     window = Platformer()
     window.setup()
     arcade.run()
+    print("You found El Do-Cookie-Rado!")
