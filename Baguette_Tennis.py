817ec59b40612e2ceae438c3652ee0e74784da6c
Anna <86629758+Arkaig@users.noreply.github.com>
2021-08-21 15:41:19 +0100

    Updating game files

--- Baguette_Tennis.py
+++ Baguette_Tennis.py
@@ -2,14 +2,13 @@
 # import modules
 import turtle # import turtle
 import winsound # sound for windows machines
-import random # need to create random direction for start / computer moves
 
-# create classes
 
-# create functions
+# game state to start game
 def start_game():
     global game_state
-    game_state = "game"
+    game_state = "game_on"
+
 
 # screen and game set-up
 window = turtle.Screen() # screen window
@@ -19,41 +18,39 @@ window.setup(width=800, height=600) # screen size
 window.tracer(0) # stops window from updating - makes faster
 
 
-# turtle object
-# ball_shape = turtle.Turtle()
-# baguette_shape = turtle.Turtle()
-  
-# registering the images as new shapes
+# registering .gif images as new shapes
 turtle.register_shape('jammie_dodger.gif')
 turtle.register_shape('baguette_1_left.gif')
 turtle.register_shape('baguette_2_right.gif')
+
 
 # score and win state initialisation
 human = 0
 computer = 0
 human_wins = False
 computer_wins = False
+game_state = "title_screen"
+
 
 # Baguette_1 - Human
 baguette_1 = turtle.Turtle()
-# baguette_1.speed(0) # speed of animation
-baguette_1.shape("baguette_1_left.gif") # sprite
+baguette_1.shape("baguette_1_left.gif") # baguette_1 image
 baguette_1.penup() # stops line tracing across screen
 baguette_1.goto(-350, 0) # starting co-ordinate
 
+
 # Baguette_2 - Computer
 baguette_2 = turtle.Turtle()
-# baguette_2.speed(0)
-baguette_2.shape("baguette_2_right.gif") # sprite
+baguette_2.shape("baguette_2_right.gif") # baguette_2 image
 baguette_2.penup() # stops line tracing across screen
 baguette_2.goto(350, 0) # starting co-ordinate
 
+
 # ball (Jammie Dodger)
 ball = turtle.Turtle()
-# ball.speed(0) # speed of animation
-ball.shape("jammie_dodger.gif")
+ball.shape("jammie_dodger.gif") # ball image
 ball.penup() # stops line tracing across screen
-ball.goto(0, 0) # starting co-ordinates
+ball.goto(0, -20) # starting co-ordinates
 ball.dx = 1 # amount pixels moving (x)
 ball.dy = 1 # amount pixels moving (y)
 
@@ -61,35 +58,34 @@ ball.dy = 1 # amount pixels moving (y)
 # scoreboard
 pen = turtle.Turtle()
 pen.speed(0)
-# pen.shape("square")
 pen.color("white")
 pen.penup() # avoids drawing line
 pen.hideturtle()
 pen.goto(0, 260)
-pen.write("Detective: 0  /  Jamie Dodger: 0", align="center", font=("San Serif", 16, "normal"))
+pen.write("Detective: 0  /  Jamie Dodger: 0", align="center", font=("Courier New", 16, "normal"))
+
 
 # movement of baguettes
 def baguette_1_up():
     y = baguette_1.ycor() # returns the y co-ordinate
-    y += 25
+    y += 25 # amount of movement 
     baguette_1.sety(y)
-
+    
 def baguette_1_down():
     y = baguette_1.ycor() # returns the y co-ordinate
-    y -= 25
+    y -= 25 # amount of movement
     baguette_1.sety(y)
 
 def baguette_2_up():
     y = baguette_2.ycor() # returns the y co-ordinate
-    y += 0.7
+    y += 0.77 # slower speed for computer to allow human to win sometimes!
     baguette_2.sety(y)
 
 def baguette_2_down():
     y = baguette_2.ycor() # returns the y co-ordinate
-    y -= 0.7
+    y -= 0.77 # slower speed for computer to allow human to win sometimes!
     baguette_2.sety(y)
 
-# need to randomise the computer movement
 
 # listening for keyboard input 
 window.listen()
@@ -97,29 +93,24 @@ window.onkeypress(start_game, "space")
 window.onkeypress(baguette_1_up, "Up")
 window.onkeypress(baguette_1_down, "Down")
 
-game_state = "splash"
-
 
 # Main game loop
 while True:
     window.update() # every time loop runs it updates screen
     
-    if game_state == "splash":
-        window.bgpic("splash.gif")
+    if game_state == "title_screen": # checks game state           
+        window.bgpic("title_screen.gif") # displays title screen
         
-    elif game_state == "game":
-        window.bgpic("nopic")
+    elif game_state == "game_on": # checks game state 
+        window.bgpic("nopic") # clears title screen to play game
         
-#     elif game_state == "gameover":
-#         window.bgpic("game_over.gif")
-#         
-        
-        # move the ball
+           
+        # moving the ball
         ball.setx(ball.xcor() + ball.dx) # setx()
         ball.sety(ball.ycor() + ball.dy) # sety()
         
 
-        # checks gameplay area
+        # checks gameplay area limits
         
         # top 
         if ball.ycor() > 270:
@@ -134,71 +125,68 @@ while True:
             winsound.PlaySound("splat_wall.wav", winsound.SND_ASYNC)
 
         # Left and right
-        if ball.xcor() > 325: # past right paddle off screen
-            ball.goto(0, 0) # back to centre
-            ball.dx = 1 # reset to initial speed
+        if ball.xcor() > 325: # past right bat and off screen
+            ball.goto(0, -20) # returns to start position
+            ball.dx = 1 # reset to initial speed and next go ball goes towards opponent
             human += 1 # player a scores a point
             pen.clear() # clears
-            pen.write("Detective: {}  /  Jamie Dodger: {}".format(human, computer), align="center", font=("San Serif", 16, "normal"))
-           
-        elif ball.xcor() < -325: # past left paddle off screen
-            ball.goto(0, 0) # back to centre
-            ball.dx = 1 # resets to initial speed
+            winsound.PlaySound("win_point.wav", winsound.SND_ASYNC)
+            pen.write("Detective: {}  /  Jamie Dodger: {}".format(human, computer), align="center", font=("Courier New", 16, "normal"))
+                       
+        elif ball.xcor() < -325: # past left bat and off screen
+            ball.goto(0, -20) # returns to start position
+            ball.dx = -1 # reset to initial speed and next go ball goes towards opponent
             computer += 1 # player b scores a point
             pen.clear() # clears
-            pen.write("Detective: {}  /  Jamie Dodger: {}".format(human, computer), align="center", font=("San Serif", 16, "normal"))    
+            winsound.PlaySound("evil_laugh.wav", winsound.SND_ASYNC)
+            pen.write("Detective: {}  /  Jamie Dodger: {}".format(human, computer), align="center", font=("Courier New", 16, "normal"))    
             
 
-        # Paddle and ball collisions
+        # Baguette and ball collisions
         if ball.xcor() < -320 and ball.ycor() < baguette_1.ycor() + 100 and ball.ycor() > baguette_1.ycor() - 100:
-            ball.dx *= -1
-            ball.dx +=0.2 # speed it up
+            ball.dx *= -1 # ball reverse direction
+            ball.dx +=0.2 # speed ball up
             winsound.PlaySound("splat_baguette.wav", winsound.SND_ASYNC)
         
         elif ball.xcor() > 320 and ball.ycor() < baguette_2.ycor() + 100 and ball.ycor() > baguette_2.ycor() - 100:
-            ball.dx *= -1
+            ball.dx *= -1 # ball reverse direction
             ball.dx -=0.2 # speed it up
             winsound.PlaySound("splat_baguette", winsound.SND_ASYNC)
 
         # computer player movements
         if baguette_2.ycor() < ball.ycor() and abs(baguette_2.ycor() - ball.ycor()) > 20:
             baguette_2_up()
-                   
-            
+                               
         elif baguette_2.ycor() > ball.ycor() and abs(baguette_2.ycor() - ball.ycor()) > 20:
             baguette_2_down()
             
 
     # score result
-
-        if human == 5:
-            turtle.clearscreen()
+        if human == 5: # checks if human score is 5
+            turtle.clearscreen() # clears screen
             human_wins = True
             break
 
-        elif computer == 5:
-            turtle.clearscreen()
-            computer_wins = True
-            break
+        elif computer == 5: # checks if computer score is 5
+            turtle.clearscreen() # clears screen
+            computer_wins = True 
+            break 
 
 
 while True:
     if human_wins:
-        window.bgcolor("black")
+        window.bgcolor("black") # background colour
         pen.penup() # avoids drawing line
         pen.hideturtle()
-        pen.goto(0, 260)
-        pen.write("You win! The clue fragment is: 0000", align="center", font=("Courier New", 16, "normal"))
+        pen.goto(0, 200) # position on screen 
+        pen.write("You win! Your clue fragment is: 0000", align="center", font=("Courier New", 16, "normal"))
+                
     elif computer_wins:
-        window.bgcolor("black")       
+        window.bgcolor("black") # background colour   
         pen.penup() # avoids drawing line
         pen.hideturtle()
-        pen.goto(0, 260)
-        pen.write("Jamie Dodger Wins!", align="center", font=("Courier New", 16, "normal"))
-            
+        pen.goto(0, 200) # position on screen         
+        pen.write("Major Jamie Dodger-McCrumbs Wins!", align="center", font=("Courier New", 16, "normal"))       
 
-
-
-        
-        
+    
         
